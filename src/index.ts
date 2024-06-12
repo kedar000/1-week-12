487e8dcc41b1aa45c62532a503a4957004778ad0
kedar000 <140879080+kedar000@users.noreply.github.com>
2024-06-12 14:34:50 +0530

    added

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+interface User  {
+    name : string;
+    age : number;
+}
+
+function sumAge(user1 : User , user2 : User){
+    return user1.age + user2.age;
+}
+
+const sum : number = sumAge({name : "kedar" , age : 22} , {name : "kk" , age : 18})
+console.log(sum);
+
