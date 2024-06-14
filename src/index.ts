fd18d07ca2d46536c998ae956ffafa0db156b74a
kedar000 <140879080+kedar000@users.noreply.github.com>
2024-06-14 19:34:45 +0530

    updated

--- src/index.ts
+++ src/index.ts
@@ -48,3 +48,36 @@ const newobj = {
 //if we change the newobject inner connect it will show error 
 
 //newobj.name = "kdascblb"
+
+//--------------------------------------
+
+
+
+type innerId = {
+    id : string,
+    name : string
+}
+type Users = {
+    [key : string] : innerId
+}
+
+//to create the below type we use this 
+
+
+//we use records to do the same thing in simple way 
+type newUsers = Record<string , innerId>
+
+
+const user : newUsers = {
+    "sdbjck" : {
+        id : "sdbjck",
+        name : "kedar"
+    },
+    "ebfow " : {
+        id : "ebfow",
+        name : "gorri"
+    }
+}
+
+///  s
+
