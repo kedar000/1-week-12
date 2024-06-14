7acb385e9aacb185c7b450ca0212bc1297395913
kedar000 <140879080+kedar000@users.noreply.github.com>
2024-06-14 11:21:38 +0530

    1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { string } from "zod";
+
 interface User  {
     name : string;
     age : number;
@@ -10,4 +12,39 @@ function sumAge(user1 : User , user2 : User){
 const sum : number = sumAge({name : "kedar" , age : 22} , {name : "kk" , age : 18})
 console.log(sum);
 
+//------------------------
 
+interface userInfo{
+    id : string;
+    username : string;
+    email : string;
+    age : string;
+    password : string;
+}
+
+type updateProps = Pick< userInfo, 'username' | 'age' | 'password' >
+
+//-------------------------------making alll optional 
+type updatePropsOptional = Partial<updateProps>
+
+
+
+function updateUser (updateInfo : userInfo){
+
+}
+
+//------readonly for an object
+
+type obj = {
+    readonly username : string;
+    readonly age : number
+}
+
+const newobj = {
+    username : "kedar",
+    age : 21
+}
+
+//if we change the newobject inner connect it will show error 
+
+//newobj.name = "kdascblb"
